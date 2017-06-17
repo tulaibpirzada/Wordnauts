b7817021f300a9aac77cb349a783a26eaa989fed
TaskeenAshraf <taskeenashraf88@gmail.com>
2017-06-17 12:59:05 +0500

    Back button of multiclue now working

--- Assets/Scripts/Events/MultiClueLevelSelectionScreenEvents.cs
+++ Assets/Scripts/Events/MultiClueLevelSelectionScreenEvents.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class MultiClueLevelSelectionScreenEvents : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void BackButtonTapped()
+    {
+        MultiClueLevelSelectionScreenController.Instance.SlideBackToMainMenu();
+    }
 }
